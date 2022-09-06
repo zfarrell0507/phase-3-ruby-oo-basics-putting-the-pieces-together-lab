b0da0b669556eeafa8b2842099e6c57f05a5d0e7
Zachary Farrell <zfarrell0507@gmail.com>
2022-09-06 14:32:02 -0400

    commit tree fc65a1a1e2a5283e8634bd44f42235a8c0156cc4

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_reader :book, :title, :turn_page
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(book)
+        @book = book
+    end
+    def title
+        @title = @book
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :shoe, :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(shoe)
+        @shoe = shoe
+    end
+    def brand
+        @brand = @shoe
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
